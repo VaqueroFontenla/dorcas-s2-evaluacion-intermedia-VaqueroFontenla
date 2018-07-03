7275a34cd7402b881263480b593512a197620dda
Loreto Vaquero <loret.vaquero@gmail.com>
2018-07-03 10:35:24 +0200

    Step3: show number input

--- js/main.js
+++ js/main.js
@@ -4,4 +4,13 @@ function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
 
-console.log('El número creado por "el juego":  ' + getRandomNumber(100));
+//console.log('El número creado por "el juego":  ' + getRandomNumber(100));
+
+var inputUSer = document.querySelector(".number")
+var btnPrueba = document.querySelector(".btn")
+
+function showNumber (e) {
+  console.log(inputUSer.value);
+}
+
+btnPrueba.addEventListener("click", showNumber);
