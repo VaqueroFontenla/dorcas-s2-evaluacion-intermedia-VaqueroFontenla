f976202671e4b8b1317ca3ffaef4a03aaaece78c
Loreto Vaquero <loret.vaquero@gmail.com>
2018-07-03 10:53:45 +0200

    Step4: start game

--- js/main.js
+++ js/main.js
@@ -1,16 +1,50 @@
 'use strict';
 
+
+// Step 2
+// function getRandomNumber(max) {
+//   return Math.ceil(Math.random() * max);
+// }
+//
+// //console.log('El número creado por "el juego":  ' + getRandomNumber(100));
+
+
+
+// Step 3
+// var inputUSer = document.querySelector(".number")
+// var btnPrueba = document.querySelector(".btn")
+//
+// function showNumber (e) {
+//   console.log(inputUSer.value);
+// }
+//
+// btnPrueba.addEventListener("click", showNumber);
+
+
+//Step 4
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
 
-//console.log('El número creado por "el juego":  ' + getRandomNumber(100));
+var randomNumber = getRandomNumber(100);
 
 var inputUSer = document.querySelector(".number")
 var btnPrueba = document.querySelector(".btn")
+var messageFeedback = document.querySelector(".feedback")
 
-function showNumber (e) {
-  console.log(inputUSer.value);
+console.log('El número creado por "el juego":  ', randomNumber);
+
+function showNumber () {
+  if (inputUSer.value == randomNumber) {
+    messageFeedback.value = "¡HAS GANADO, CAMPEONA!";
+    return(messageFeedback.value)
+  } else if (inputUSer.value > randomNumber) {
+    messageFeedback.value = "demasiado alto";
+    return(messageFeedback.value)
+} else {
+  messageFeedback.value = "demasiado bajo";
+  return(messageFeedback.value)
+}
 }
 
 btnPrueba.addEventListener("click", showNumber);
