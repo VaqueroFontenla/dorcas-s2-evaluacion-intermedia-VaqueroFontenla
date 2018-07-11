89c0b0f0511e1caf2e152ba7fcec5a454f929456
Loreto Vaquero <loret.vaquero@gmail.com>
2018-07-11 08:38:42 +0200

    Add conter

--- js/main.js
+++ js/main.js
@@ -9,23 +9,22 @@ var randomNumber = getRandomNumber(100);
 
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
-
+}
 console.log('El número creado por "el juego":  ', randomNumber);
 
 function showNumber () {
   conterStart = conterStart +1;
   conterChance.innerHTML= conterStart;
-  if (inputUSer.value === randomNumber) {
-    messageFeedback.value = '¡HAS GANADO, CAMPEONA!';
-    return(messageFeedback.value)
+  if (inputUSer.value < randomNumber) {
+    messageFeedback.value = '¡demasiado bajo!';
+    return(messageFeedback.value);
   } else if (inputUSer.value > randomNumber) {
     messageFeedback.value = 'demasiado alto';
-    return(messageFeedback.value)
-} else {
-  messageFeedback.value = 'demasiado bajo';
-  return(messageFeedback.value)
-}
-
+    return(messageFeedback.value);
+  } else {
+    messageFeedback.value = '¡HAS GANADO, CAMPEONA!';
+    return(messageFeedback.value);
+  }
 }
 
 btnPrueba.addEventListener('click', showNumber);
